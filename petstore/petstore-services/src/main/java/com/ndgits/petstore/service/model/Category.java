b9913ceb170a400fedde65018664733b879a0968
Evgeni Reingold <evgeni_reingold@hotmail.com>
2018-04-01 20:18:46 -0400

    clean repo

--- petstore/petstore-services/src/main/java/com/ndgits/petstore/service/model/Category.java
+++
@@ -1,8 +0,0 @@
-package com.ndgits.petstore.service.model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Category extends BaseM {
-
-}
--- petstore/petstore-services/src/main/java/com/ndgits/petstore/service/repo/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ndgits.petstore.service.repo;
-
-import com.ndgits.petstore.service.model.Category;
-
-public interface CategoryRepository extends BaseRepository<Category> {
-
-}
